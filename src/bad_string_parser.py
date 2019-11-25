643a838adbb41370e99103f2c0519437ee346deb
Nolan <noltron000@gmail.com>
2019-11-25 13:06:52 -0800

    [add] bad_string_parser docstrings

--- src/bad_string_parser.py
+++ src/bad_string_parser.py
@@ -18,49 +18,77 @@ def cleanse_markdown(data):
 
 def remove_metadata(dirty):
 	if re.search(r'{@chance .+?}', dirty):
+		'''
+		Chances should be in backticks
+		and must have a (%) percentile sign.
+		In some rare cases, a (‰) permille sign
+		may be more appropriate instead.
+		'''
 		# remove typing.
 		expr = r'(?<={@chance\s).+(?=})'
 		dirty = re.search(expr, dirty).group()
-		# add percentage.
-		dirty += ' percent'
+		# add code ticks to specify statistics or randomness.
+		# then add a percentage.
+		dirty += f'`{dirty}%`'
 
 	elif re.search(r'{@condition .+?}', dirty):
+		'''
+		Conditions should be emboldened; they are more
+		important to notice than other game statistics.
+		'''
 		# remove typing.
 		expr = r'(?<={@condition\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
-		# || implies or in this json.
+		# || implies a semantic split in the json.
+		# in this case, we just want the final item.
 		expr = r'.+\|\|'
 		dirty = re.sub(expr, '', dirty)
-		# italicize conditions
-		# TODO italics or bold??
+		# embolden conditions.
 		dirty = f'**{dirty}**'
 
 	elif re.search(r'{@creature .+?}', dirty):
+		'''
+		Creatures, races, classes, and their various subtypes
+		should be modestly distinguishable so they can be read
+		as a proper game mechanic versus their english meaning.
+		'''
 		# remove typing.
 		expr = r'(?<={@creature\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
-		# || implies or in this json.
+		# || implies a semantic split in the json.
+		# in this case, we just want the final item.
 		expr = r'.+\|\|'
 		dirty = re.sub(expr, '', dirty)
 		# remove creature data.
 		expr = r'\|.+?\|.+'
 		dirty = re.sub(expr, '', dirty)
+		# add italics.
+		dirty = f'*{dirty}*'
 
 	elif re.search(r'{@damage .+?}', dirty):
+		'''
+		==TODO==
+		add docstring
+		'''
 		# remove typing.
 		expr = r'(?<={@damage\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
-		# add code ticks to specify a dice roll.
+		# add code ticks to specify statistics or randomness.
 		dirty = f'`{dirty}`'
 
 	elif re.search(r'{@dice .+?}', dirty):
+		'''
+		==TODO==
+		add docstring
+		fix html symbols
+		'''
 		# remove typing.
 		expr = r'(?<={@dice\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
 		# remove dice average indicator from string.
 		expr = r'\|\d+'
 		dirty = re.sub(expr, '', dirty)
-		# Space out adding, subtracting, multiplying, dividing.
+		# add padding to mathematical operators.
 		expr = r'(?<=\d)\+(?=\d)'
 		dirty = re.sub(expr, ' + ', dirty)
 		expr = r'(?<=\d)\-(?=\d)'
@@ -69,10 +97,14 @@ def remove_metadata(dirty):
 		dirty = re.sub(expr, ' &times; ', dirty)
 		expr = r'(?<=\d)[\÷|\/](?=\d)'
 		dirty = re.sub(expr, ' &divide; ', dirty)
-		# add code ticks to specify a dice roll.
+		# add code ticks to specify statistics or randomness.
 		dirty = f'`{dirty}`'
 
 	elif re.search(r'{@filter .+?}', dirty):
+		'''
+		==TODO==
+		add docstring
+		'''
 		# remove typing.
 		expr = r'(?<={@filter\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
@@ -81,6 +113,10 @@ def remove_metadata(dirty):
 		dirty = re.sub(expr, '', dirty)
 
 	elif re.search(r'{@i .+?}', dirty):
+		'''
+		==TODO==
+		add docstring
+		'''
 		# remove typing.
 		expr = r'(?<={@i\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
@@ -88,61 +124,96 @@ def remove_metadata(dirty):
 		dirty = f'*{dirty}*'
 
 	elif re.search(r'{@hit .+?}', dirty):
+		'''
+		==TODO==
+		add docstring
+		'''
 		# remove typing.
 		expr = r'(?<={@hit\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
-		# add code ticks to specify a dice roll.
+		# add code ticks to specify statistics or randomness.
 		dirty = f'`{dirty}`'
 
 	elif re.search(r'{@item .+?}', dirty):
+		'''
+		Items, spells, and other miscellaneous game mechanics
+		should be modestly distinguishable so they can be read
+		as a proper game mechanic versus their english meaning.
+		'''
 		# remove typing.
 		expr = r'(?<={@item\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
 		# remove all these extra metadata after |
 		expr = r'\|.+'
 		dirty = re.sub(expr, '', dirty)
+		# add italics.
+		dirty = f'*{dirty}*'
 
 	elif re.search(r'{@race .+?}', dirty):
+		'''
+		Creatures, races, classes, and their various subtypes
+		should be modestly distinguishable so they can be read
+		as a proper game mechanic versus their english meaning.
+		'''
 		# remove typing.
 		expr = r'(?<={@race\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
-		# || implies or in this json.
+		# || implies a semantic split in the json.
+		# in this case, we just want the final item.
 		expr = r'.+\|\|'
 		dirty = re.sub(expr, '', dirty)
+		# add italics.
+		dirty = f'*{dirty}*'
 
 	elif re.search(r'{@scaledice .+?}', dirty):
+		'''
+		==TODO==
+		add docstring
+		'''
 		# remove typing.
 		expr = r'(?<={@scaledice\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
 		# remove all these extra metadata after |
 		expr = r'.+\|'
 		dirty = re.sub(expr, '', dirty)
-		# add code ticks to specify a dice roll.
+		# add code ticks to specify statistics or randomness.
 		dirty = f'`{dirty}`'
 
 	elif re.search(r'{@sense .+?}', dirty):
-		print(dirty)
+		'''
+		==TODO==
+		add docstring
+		'''
 		# remove typing.
 		expr = r'(?<={@sense\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
 		# remove all these extra metadata after |
 		expr = r'.+\|'
 		dirty = re.sub(expr, '', dirty)
-		# add code ticks to specify a dice roll.
+		# add code ticks to specify statistics or randomness.
 		dirty = f'`{dirty}`'
 
 	elif re.search(r'{@skill .+?}', dirty):
+		'''
+		==TODO==
+		add docstring
+		'''
 		# remove typing.
 		expr = r'(?<={@skill\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
 
 	elif re.search(r'{@spell .+?}', dirty):
+		'''
+		==TODO==
+		add docstring
+		'''
 		# remove typing.
 		expr = r'(?<={@spell\s).+?(?=})'
 		dirty = re.search(expr, dirty).group()
-		# embolden other spell names
+		# ==TODO==
+		# add markdown link to other spell
 		slug = slugify(dirty)
-		#TODO add markdown link to other spell
+		# add italics.
 		dirty = f'[*{dirty}*](./{slug})'
 
 	else:
