1d3b540368a920511714f17ea0e29f520fbca5e5
Nolan <noltron000@gmail.com>
2019-11-27 15:29:33 -0800

    [doc] library.py

--- src/library.py
+++ src/library.py
@@ -10,10 +10,14 @@ pass
 class Library:
 	'''
 	A library contains many books in an orderly fashion.
+	The library is a more abstract idea, because
+	it is, by definition, a collection of books.
+	In most cases, you will want every book, so this will
+	be a collection of all the available books from WOTC.
 	'''
 	def __init__(self):
-		self.name = None
-		self.slug = None
+		# Honestly, this class is a glorified object.
+		# You can find books by name and view their contents.
 		self.books = {}
 
 
